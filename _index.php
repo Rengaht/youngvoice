de3541ff333fbb3e51054e39f755a96215ef1ebc
Reng Tsai <rengah0@gmail.com>
2019-03-08 18:48:54 +0800

    update

---
+++ _index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+	<script>
+		setTimeout(function () {
+			window.location = "https://game.youngvoice.tw";
+	    }, 100); //will
+	</script>
+</body>
+</html>
